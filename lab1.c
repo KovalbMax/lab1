9bfa1798c2fe3c0bedc1de594a64e60c6fba78c5
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-02-15 11:51:29 +1000

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+include<stdio.h>
 #include<math.h>
 
 int main()
 {
-    int n;
-    int a, b, c, d;
+    int n=0;
+    int a, b, c, d, o;
     double x1 ,x2;
-    do {
+    
     printf("Напишите a,b,c через пробел - ");
     scanf("%d%d%d",&a,&b,&c);
     d=b*b - 4*a*c;
@@ -23,6 +23,30 @@ int main()
     x2=(-b-sqrt(d))/(2*a);
     
     printf("x1=%.2lf\nx2=%.lf",x1,x2);}
+    
+    do {
+    printf("Если хотите повторить программу напишите 1, если нет - 2");
+    scanf("%d",&o);
+    if (o==1){
+    printf("Напишите a,b,c через пробел - ");
+    scanf("%d%d%d",&a,&b,&c);
+    d=b*b - 4*a*c;
+    if (d<0){
+    printf("Дискриминант меньше 0");
     }
+    if (d==0){
+    x1=-b/(2*a);
+    
+    printf("x1=%lf",x1);
+    }
+    if (d>0){
+    x1=(-b+sqrt(d))/(2*a);
+    x2=(-b-sqrt(d))/(2*a);
+    
+    printf("x1=%.2lf\nx2=%.lf",x1,x2);}}
+    else {
+    break;}
+    } while(n<5);
+    
     return 0;
 }
