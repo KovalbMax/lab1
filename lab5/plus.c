282db9adecde2e668537baebf4e05be0d33ca749
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-03-13 13:15:34 +1000

    2

--- lab5/plus.c
+++ lab5/plus.c
@@ -1,35 +1,34 @@
-double **sum(double **a, double **b, int n) {
-    double **result = (double **)malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) {
-        result[i] = (double *)malloc(n * sizeof(double));
-        for (int j = 0; j < n; j++) {
-            result[i][j] = a[i][j] + b[i][j];
-        }
-    }
-    return result;
-}
+//Написать функцию, вычисляющую сумму, разность или произведение данных матриц
+//   (их передать по указателю) и возвращающую указатель на массив-результат. 
+//  Разместить эту функцию в отдельном модуле (отличном от того, где располагается
+//  функция main()). Выдать на консоль значения полученного вектора. 
 
-double **multiply(double **a, double **b, int n) {
-    double **result = (double **)malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) {
-        result[i] = (double *)malloc(n * sizeof(double));
-        for (int j = 0; j < n; j++) {
-            result[i][j] = 0;
-            for (int k = 0; k < n; k++) {
-                result[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-    return result;
+int n;
+double sum(double a[n][n], double b[n][n]){
+	for (int i=0; i<n; i++){
+		for (int j=0; j<n; j++){
+			return a[i][j] + b[i][j];
+			
+		}
+	}
 }
-
-double **subtract(double **a, double **b, int n) {
-    double **result = (double **)malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) {
-        result[i] = (double *)malloc(n * sizeof(double));
-        for (int j = 0; j < n; j++) {
-            result[i][j] = a[i][j] - b[i][j];
-        }
-    }
-    return result;
+double proiz(double a[n][n], double b[n][n]){
+	for (int i=0; i<2; i++){
+		for (int j=0; j<2; j++){
+			for (int k=0; k<2; k++) {
+				return a[i][k] * b[k][j];
+				 
+			
+			}
+	
+		}
+	}
+}
+double minus(double a[n][n], double b[n][n]){
+	for (int i=0; i<n; i++){
+		for (int k=0; k<n;k++){
+			return a[i][k] - b[i][k];
+			
+		}
+	}
 }
