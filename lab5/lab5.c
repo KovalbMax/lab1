1388b62b7affe74e834430405411ac7c7b90270a
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-03-22 19:27:39 +1000

    5

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,7 +1,6 @@
 
 #include <stdlib.h>
 #include <malloc.h>
-#include"plus.h"
 /* Разместить в динамической памяти 2 массива – 2 матрицы размерностью 
 n на n с элементами типа double (значения n и элементы массивов ввести с консоли).
  А также ввести с консоли знак операции: '+', '-' или '*'.
@@ -12,59 +11,35 @@ n на n с элементами типа double (значения n и элем
 
 int main(int argc, char *argv[]) {
 	int n;
-	double p[n][n];
-	double m[n][n];
-	double o[n][n];
-	printf("Vvedite razmer");
+	int znac;
+	printf("Vvedite razmernost - ");
 	scanf("%d", &n);
-	double **matrix1 = (double **)malloc(n * sizeof(double *));
-	double **matrix2 = (double **)malloc(n * sizeof(double *));
-	for (int i=0; i<n ; i++){
-		matrix1[i]=(double*)malloc(n*sizeof(double));
-		matrix2[i]=(double*)malloc(n*sizeof(double));
+	double **matrix1; double **matrix2;
+	matrix1=(double**)malloc(n*sizeof(double*));
+	matrix2=(double**)malloc(n*sizeof(double*));
+	for (int i=0; i<n; i++){
+		matrix1[i]=(double**)malloc(n*sizeof(double));
+		matrix2[i]=(double**)malloc(n*sizeof(double));
 	}
-	printf("Vvedite elementi 1-oi matrici");
+	printf("Vvedite znacheniya 1 matrici - ");
 	for (int i=0; i<n; i++){
 		for (int j=0; j<n; j++){
-			scanf("%lf", &matrix1[i][j]);
+			scanf("%lf ", &matrix1[i][j]);	
 		}
 	}
-	printf("Vvedite elementi 2-oi matrici");
+	printf("Vvedite znacheniya 2 matrici - ");
 	for (int i=0; i<n; i++){
-		for (int j=0; j<n;j++){
-			scanf("%lf", &matrix2[i][j]);
+		for (int j=0; j<n; j++){
+			scanf("%lf", &matrix2[i][j]);	
 		}
 	}
-	for (int i=0; i<n;i++){
+	printf("Vvedite znac operacii(+,-,*) - ");
+	scanf("%c", &znac);
+	for (int i=0; i<n; i++){
 		free(matrix1[i]);
 		free(matrix2[i]);
 	}
 	free(matrix1);
 	free(matrix2);
-	printf("Vvedite znac(+,-,*)");
-	char znac; 
-	scanf("%c", &znac);
-	if (znac=='+'){
-		for (int i=0; i<n ; i++){
-			for (int k=0; k<n ; k++){
-				p[i][k] += sum(matrix1[n][n],matrix2[n][n]);
-			}
-		}
-		
-	}
-	else if(znac=='-'){
-		for (int i=0; i<n ; i++){
-			for (int j=0; j<n ; j++){
-				p[i][j] += minus(matrix1[n][n],matrix2[n][n]);
-			}
-		}
-	}
-	else if(znac=='*'){
-		for (int i=0; i<n ; i++){
-			for (int j=0; j<n ; j++){
-				p[i][j] += proiz(matrix1[n][n],matrix2[n][n]);
-			}
-		}
-	}
 	return 0;
 }
