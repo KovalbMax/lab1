b38712e8960a7a725546a5b8df959a0b5ad4de72
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-03-27 13:35:18 +1000

    2

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,53 +1,59 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include "operations.h"
-/* Разместить в динамической памяти 2 массива – 2 матрицы размерностью 
-n на n с элементами типа double (значения n и элементы массивов ввести с консоли).
- А также ввести с консоли знак операции: '+', '-' или '*'.
-  Написать функцию, вычисляющую сумму, разность или произведение данных матриц
-   (их передать по указателю) и возвращающую указатель на массив-результат. 
-  Разместить эту функцию в отдельном модуле (отличном от того, где располагается
-   функция main()). Выдать на консоль значения полученного вектора. */
 
 int main() {
-	int n;
-	char znac;
-	printf("Vvedite razmernost -");
-	double **matrix1; double **matrix2;
-	matrix1=(double**)malloc(n*sizeof(double*));
-	matrix2=(double**)malloc(n*sizeof(double*));
-	for (int i=0; i<n; i++){
-		matrix1[i]=(double*)malloc(n*sizeof(double));
-		matrix2[i]=(double*)malloc(n*sizeof(double));
-	}
-	printf("Vvedite znacheniya 1 matrici \n- ");
-	for (int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
-			scanf("%lf ", &matrix1[i][j]);	
-		}
-	}
-	printf("Vvedite znacheniya 2 matrici - \n");
-	for (int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
-			scanf("%lf", &matrix2[i][j]);	
-		}
-	}
-	printf("VVedite znac operacii(+,-,*) - \n");
-	double **result = oper(matrix1, matrix2, n, znac);
-	printf("Rezultat - \n");
-	for (int i = 0; i < n; i++) {
-       for (int j = 0; j < n; j++) {
-           printf("%.2f", result[i][j]);
-    	}
-	}
-	for (int i=0; i<n; i++){
-		free(matrix1[i]);
-		free(matrix2[i]);
-		free(result[i]);
-	}
-	free(matrix1);
-	free(matrix2);
-	free(result);
-	return 0;
+    int n;
+    char znac;
+    printf("Vvedite razmernost - ");
+    scanf("%d", &n);
+
+    double** matrix1;
+    double** matrix2;
+
+    matrix1 = (double**)malloc(n * sizeof(double*));
+    matrix2 = (double**)malloc(n * sizeof(double*));
+
+    for (int i = 0; i < n; i++) {
+        matrix1[i] = (double*)malloc(n * sizeof(double));
+        matrix2[i] = (double*)malloc(n * sizeof(double));
+    }
+
+    printf("Vvedite znacheniya 1 matrici:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%lf", &matrix1[i][j]);
+        }
+    }
+
+    printf("Vvedite znacheniya 2 matrici:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%lf", &matrix2[i][j]);
+        }
+    }
+
+    printf("VVedite znac operacii(+,-,*) - ");
+    scanf(" %c", &znac);
+
+    double **result = oper(matrix1, matrix2, n, znac);
+
+    printf("Rezultat:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%.2f ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < n; i++) {
+        free(matrix1[i]);
+        free(matrix2[i]);
+        free(result[i]);
+    }
+    free(matrix1);
+    free(matrix2);
+    free(result);
+
+    return 0;
 }
--- lab5/operations.c
+++ lab5/operations.c
@@ -1,26 +1,24 @@
 #include <stdlib.h>
 #include <malloc.h>
-/*Написать функцию, вычисляющую сумму, разность или произведение данных матриц
-   (их передать по указателю) и возвращающую указатель на массив-результат. 
-  Разместить эту функцию в отдельном модуле (отличном от того, где располагается
-функция main()). Выдать на консоль значения полученного вектора. */
-int** operations(double** matrix1, double** matrix2, int n ,char znac) {
-    int** result = (double**)malloc(n * sizeof(double*));
-    for (int i = 0; i < rows; i++) {
+double** oper(double** matrix1, double** matrix2, int n ,char znac) {
+    double** result = (double**)malloc(n * sizeof(double*));
+    for (int i = 0; i < n; i++) {
         result[i] = (double*)malloc(n * sizeof(double));
-        for (int j = 0; j <n; j++) {
+    }
+    for (int i=0; i<n ; i++){
+       for (int j = 0; j <n; j++) {
             if (znac == '+') {
                 result[i][j] = matrix1[i][j] + matrix2[i][j];
             } else if (znac == '-') {
                 result[i][j] = matrix1[i][j] - matrix2[i][j];
             } else if (znac == '*') {
                 result[i][j] = 0;
-                for (int k = 0; k < cols; k++) {
+                for (int k = 0; k < n; k++) {
                     result[i][j] += matrix1[i][k] * matrix2[k][j];
                 }
             }
         }
     }
-    unt l;
+        
     return result;
 }
