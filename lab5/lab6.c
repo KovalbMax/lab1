730ee4603fe76e35c2b1832eafae56c92d10670e
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-03-30 23:53:01 +1000

    1

--- lab5/lab6.c
+++ lab5/lab6.c
@@ -1,9 +1,16 @@
-#include<stdio>
+#include <stdio.h>
+#include <stdlib.h>
 /*Создать текстовый файл с записями следующего вида:Иванов Петр Сергеевич 1975
 Сидоров Николай Андреевич 1981….Воробьянинов Ипполит Матвеевич 1978
  Прочитать данные из этого файла и записать в другой только те строки, 
  которые относятся к родившимся позднее 1980 года.*/
 int main(){
-	FILE *imena;
-	imena=fopen("imena.docx", "rt")
+	char str[]="1, 2, 3"; 
+	FILE *names = fopen("imena.txt", "r");
+	FILE *newnames=fopen("newimena.txt", "w");
+	fputs(str, newnames);
+	fclose(newnames);
+	fclose(names);
+	return 0;
+	
 }
