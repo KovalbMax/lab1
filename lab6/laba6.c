ce5238ee6224cce7df2b3245608646079f8b44bb
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-03-31 20:29:23 +1000

    1

--- lab6/laba6.c
+++ lab6/laba6.c
@@ -2,16 +2,23 @@
 //Иванов Петр Сергеевич 1975 Сидоров Николай Андреевич 1981….
 //Воробьянинов Ипполит Матвеевич 1978 Прочитать данные из этого файла и 
 //записать в другой только те строки, которые относятся к родившимся позднее 1980 года.
-
 #include <stdio.h>
+#include <string.h>
 int main(){
-	char line[1000];
-	FILE *newimena=fopen("newnames.txt", "a");
-	FILE *imena=fopen("imena.txt", "r");
-	fgets(line, 1000, imena);
-	printf("%s", line);
+	char str[50];
+	FILE * names= fopen("imena.txt", "r");
+	FILE * newnames = fopen("newna.txt", "a");
+	while (fgets(str, 50, names) != NULL){
+		int length = strlen(str);
+    	int index = length - 1;
+    	printf("Last: %c", str[index]);
+		
+    }	
+			
+
+			
 	
-	fclose(imena);
-	fclose(newimena);
+	fclose(names);
+	fclose(newnames);
 	return 0;
 }
