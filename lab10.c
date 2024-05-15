255b78636107d21f0363fc39dbe79633bb0af61a
KovalbMax <160006280+KovalbMax@users.noreply.github.com>
2024-05-15 12:10:48 +1000

    10

--- lab10.c
+++ lab10.c
@@ -1,20 +1,17 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
-/*Написать программу вычисления максимального числа из введенных пользователем в виде одной строки. 
-Использовать для этого макрос (без функции), на вход которому подается массив чисел и их количество. 
-*/ 
-#define max(arr, size) ({\ 
- double r = 0, res = 0; \ 
- for(int i = 0; i < size-1; i++){ \ 
-  if(arr[i] < arr[i+1]){ \ 
-   r = arr[i+1];}} \ 
- res = r; \ 
- printf("\nresult: %.2lf", res);}) 
- 
+
+#define max(arr, size) ({ \ 
+    double res = arr[0]; \ 
+    for(int i = 0; i < size; i++){ \ 
+        if(arr[i] > res){ \ 
+            res = arr[i];}} \ 
+    printf("\nresult: %.2lf", res);}) 
+
 int main() { 
      
-    int n = 0, i; 
+    int n = 0; 
     double nums[16]; 
     char s[64]; 
     const char cu[2] = " "; 
